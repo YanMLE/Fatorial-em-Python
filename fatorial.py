59c21177462c54b140c9046ab59e021fec29f100
Yan Vinícius <158772389+YanMLE@users.noreply.github.com>
2024-04-05 10:21:55 -0300

    adicionei alguns casos

--- fatorial.py
+++ fatorial.py
@@ -2,6 +2,10 @@ x = int(input("escolha um número: "))
 def fatorial(x):
     if x == 1:
         return 1
+    elif x == 0:
+        return 1
+    elif x < 0:
+        return "Isso aí existe, não, meu fi"
     else:
         return x * fatorial(x-1)
 
